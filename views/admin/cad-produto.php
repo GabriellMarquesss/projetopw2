f5bc6d61ddbc1160876c2545228bd3d01f72ee30
GabriellMarquesss <marquesgabriel768@gmail.com>
2022-07-21 20:10:17 -0300

    aula dia 21/07/2022 codigo com vitrine

--- views/admin/cad-produto.php
+++ views/admin/cad-produto.php
@@ -56,7 +56,7 @@ include_once "menu.php";
                         unlink($dir . $produto->getImagem());
                     }
                     $ext = strtolower(substr($_FILES['imagem']['name'],-4)); //Pegando extensão do arquivo
-                    $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+                    $new_name =  date("Y.m.d-H.i.s").$ext; //Definindo um novo nome para o arquivo
                     $dir = './imagens/produtos/'; //Diretório para uploads
                     move_uploaded_file($_FILES['imagem']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
                     $produto->setImagem($new_name);
--- views/admin/login.php
+++ views/admin/login.php
@@ -92,13 +92,13 @@ if (isset($_POST['enviar'])){
 
 
     <!-- Custom styles for this template -->
-    <link href="./css/signin.css" rel="stylesheet">
+    <link href="css/signin.css" rel="stylesheet">
 </head>
 <body class="text-center">
 
 <main class="form-signin w-100 m-auto">
     <form method="post" action="login.php">
-        <img class="mb-4" src="./imagens/bootstrap-logo.svg" alt="" width="72" height="57">
+        <img class="mb-4" src="imagens/bootstrap-logo.svg" alt="" width="72" height="57">
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
         <div class="form-floating">
